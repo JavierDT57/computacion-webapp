1898964aaee6b6a980821591c1600a0c883a9396
JavierDT57 <javierdolorestolentino@gmail.com>
2024-02-18 20:08:51 -0600

    PLantilla basica para el form alumno

--- src/app/partials/registro-alumnos/registro-alumnos.component.ts
+++ src/app/partials/registro-alumnos/registro-alumnos.component.ts
@@ -1,11 +1,73 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-registro-alumnos',
   templateUrl: './registro-alumnos.component.html',
   styleUrls: ['./registro-alumnos.component.scss']
 })
-export class RegistroAlumnosComponent {
+export class RegistroAlumnosComponent implements OnInit{
   @Input() rol:string = "";
+
+  //JSON para alumnos
+  public alumno:any = {};
+  public editar:boolean = false;
+  public errors:any = {};
+  //Para contraseñas
+  public hide_1: boolean = false;
+  public hide_2: boolean = false;
+  public inputType_1: string = 'password';
+  public inputType_2: string = 'password';
+
+  constructor(
+
+  ) { }
+
+
+  ngOnInit(): void {
+
+  }
+
+
+   //funciones de botones del form
+   public regresar(){
+
+   }
+
+   public registrar(){
+
+
+   }
+
+   public actualizar(){
+
+   }
+
+
+  //Funciones para password
+  showPassword()
+  {
+    if(this.inputType_1 == 'password'){
+      this.inputType_1 = 'text';
+      this.hide_1 = true;
+    }
+    else{
+      this.inputType_1 = 'password';
+      this.hide_1 = false;
+    }
+  }
+
+  showPwdConfirmar()
+  {
+    if(this.inputType_2 == 'password'){
+      this.inputType_2 = 'text';
+      this.hide_2 = true;
+    }
+    else{
+      this.inputType_2 = 'password';
+      this.hide_2 = false;
+    }
+  }
+
+
 
 }
