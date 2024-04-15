ca214da998191b6109042c2dcc4e0ab07d280184
Javier <javierdolorestolentino@gmail.com>
2024-04-15 13:11:05 -0600

    creacion del componente eliminaruser y sus importaciones

---
+++ src/app/modals/eliminar-user-modal/eliminar-user-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eliminar-user-modal',
+  templateUrl: './eliminar-user-modal.component.html',
+  styleUrls: ['./eliminar-user-modal.component.scss']
+})
+export class EliminarUserModalComponent {
+
+}
