b2bf0ea8da24eda3b87654be33ba18841308d9cc
Javier <javierdolorestolentino@gmail.com>
2024-04-14 20:51:13 -0600

    anadiendo funcion para editar maestros

--- src/app/partials/registro-maestros/registro-maestros.component.ts
+++ src/app/partials/registro-maestros/registro-maestros.component.ts
@@ -120,8 +120,27 @@ public materias:any[]= [
   }
 
   public actualizar(){
+    //Validación
+    this.errors = [];
 
+    this.errors = this.maestrosService.validarMaestro(this.maestro, this.editar);
+    if(!$.isEmptyObject(this.errors)){
+      return false;
+    }
+    console.log("Pasó la validación");
+
+    this.maestrosService.editarMaestro(this.maestro).subscribe(
+      (response)=>{
+        alert("Maestro editado correctamente");
+        console.log("Maestro editado: ", response);
+        //Si se editó, entonces mandar al home
+        this.router.navigate(["home"]);
+      }, (error)=>{
+        alert("No se pudo editar el maestro");
+      }
+    );
   }
+
 
 
     //Función para detectar el cambio de fecha
