053a7f7349ec94695b175a37245e0e6746966978
Javier <javierdolorestolentino@gmail.com>
2024-04-14 22:14:36 -0600

    anadiendo funcion para seleccionar rol de alumnos y funciones para conseguir el rol, editar y eliminar alumno

--- src/app/screens/registro-screen/registro-screen.component.ts
+++ src/app/screens/registro-screen/registro-screen.component.ts
@@ -90,7 +90,22 @@ export class RegistroScreenComponent implements OnInit{
             alert("No se pudieron obtener los datos del usuario para editar");
           }
         );
-      }//TODO: Agregar función para obtener alumno por ID
+      }else if(this.rol == "alumno"){
+        this.alumnosService.getAlumnoByID(this.idUser).subscribe(
+          (response)=>{
+            this.user = response;
+            //Agregamos valores faltantes
+            this.user.first_name = response.user.first_name;
+            this.user.last_name = response.user.last_name;
+            this.user.email = response.user.email;
+            this.user.tipo_usuario = this.rol;
+            this.isAlumno = true;
+            console.log("Datos alumno: ", this.user);
+          }, (error)=>{
+            alert("No se pudieron obtener los datos del usuario para editar");
+          }
+        );
+      }
     }
 
     public radioChange(event: MatRadioChange) {
