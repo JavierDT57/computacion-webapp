e5e9f419685f032c7532cb5040c7ee9dc416914f
Javier <javierdolorestolentino@gmail.com>
2024-04-26 00:08:53 -0600

    copiando formato de maestros para materias, para poder validar campos en el front

--- src/app/partials/registro-materias/registro-materias.component.ts
+++ src/app/partials/registro-materias/registro-materias.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { MaestrosService } from 'src/app/services/maestros.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FacadeService } from 'src/app/services/facade.service';
+
+declare var $: any;
 
 @Component({
   selector: 'app-registro-materias',
@@ -6,5 +12,164 @@ import { Component } from '@angular/core';
   styleUrls: ['./registro-materias.component.scss']
 })
 export class RegistroMateriasComponent {
+  @Input() rol: string = "";
+  @Input() datos_user: any = {};
 
+
+  public maestro:any= {};
+  public token: string = "";
+  public errors:any={};
+  public editar:boolean = false;
+  public idUser: Number = 0;
+  //Check
+  public valoresCheckbox: any = [];
+  public materias_json: any [] = [];
+
+//materias y areas de investigacion para form maestros
+//Para el select
+public areas: any[] = [
+  {value: '1', viewValue: 'Ingenieria en Ciencias de la Computacion'},
+  {value: '2', viewValue: 'Licenciatura en Ciencias de la Computacion'},
+  {value: '3', viewValue: 'Ingenieria en Tecnologias de la Informacion'},
+];
+
+public materias:any[]= [
+  {value: '1', nombre: 'Lunes'},
+  {value: '2', nombre: 'Martes'},
+  {value: '3', nombre: 'Miercoles'},
+  {value: '4', nombre: 'Jueves'},
+  {value: '5', nombre: 'Viernes'},
+  {value: '6', nombre: 'Sabado'},
+];
+
+
+
+
+  constructor(
+    private location : Location,
+    private maestrosService: MaestrosService,
+    private router: Router,
+    public activatedRoute: ActivatedRoute,
+    private facadeService: FacadeService
+  ) { }
+
+  ngOnInit(): void {
+    //El primer if valida si existe un parámetro en la URL
+    if(this.activatedRoute.snapshot.params['id'] != undefined){
+      this.editar = true;
+      //Asignamos a nuestra variable global el valor del ID que viene por la URL
+      this.idUser = this.activatedRoute.snapshot.params['id'];
+      console.log("ID User: ", this.idUser);
+      //Al iniciar la vista asignamos los datos del user
+      this.maestro = this.datos_user;
+    }else{
+      this.maestro = this.maestrosService.esquemaMaestro();
+      this.maestro.rol = this.rol;
+      this.token = this.facadeService.getSessionToken();
+    }
+    //Imprimir datos en consola
+    console.log("Maestro: ", this.maestro);
+  }
+
+
+  //funciones de botones del form
+  public regresar(){
+    this.location.back();
+  }
+
+  public registrar(){
+
+        //Validar
+        this.errors = [];
+
+        this.errors = this.maestrosService.validarMaestro(this.maestro, this.editar);
+        if(!$.isEmptyObject(this.errors)){
+          return false;
+        }
+
+        // TODO:Después registraremos admin
+        //validar contrasena
+        if(this.maestro.password == this.maestro.confirmar_password){
+          this.maestrosService.registrarMaestro(this.maestro).subscribe(
+            (response: any) => {
+              alert("Usuario registrado correctamente");
+              console.log("Usuario registrado correctamente: ", response);
+              this.router.navigate(['/']);
+            },
+            (error: any) => {
+              alert("Error al registrar usuario");
+            }
+          );
+
+        }else{
+          alert("Las contraseñas no coinciden");
+          this.maestro.password = "";
+          this.maestro.confirmar_password = "";
+        }
+
+  }
+
+  public actualizar(){
+    //Validación
+    this.errors = [];
+
+    this.errors = this.maestrosService.validarMaestro(this.maestro, this.editar);
+    if(!$.isEmptyObject(this.errors)){
+      return false;
+    }
+    console.log("Pasó la validación");
+
+    this.maestrosService.editarMaestro(this.maestro).subscribe(
+      (response)=>{
+        alert("Maestro editado correctamente");
+        console.log("Maestro editado: ", response);
+        //Si se editó, entonces mandar al home
+        this.router.navigate(["home"]);
+      }, (error)=>{
+        alert("No se pudo editar el maestro");
+      }
+    );
+  }
+
+
+    //Función para detectar el cambio de fecha
+  public changeFecha(event :any){
+    console.log(event);
+    console.log(event.value.toISOString());
+
+    this.maestro.fecha_nacimiento = event.value.toISOString().split("T")[0];
+    console.log("Fecha: ", this.maestro.fecha_nacimiento);
+  }
+
+  //Checkbox para materias
+
+  public checkboxChange(event:any){
+    //console.log("Evento: ", event);
+    if(event.checked){
+      this.maestro.materias_json.push(event.source.value)
+    }else{
+      console.log(event.source.value);
+      this.maestro.materias_json.forEach((materia, i) => {
+        if(materia == event.source.value){
+          this.maestro.materias_json.splice(i,1)
+        }
+      });
+    }
+    console.log("Array materias: ", this.maestro);
+  }
+
+
+
+  public revisarSeleccion(nombre: string){
+    if(this.maestro.materias_json){
+      var busqueda = this.maestro.materias_json.find((element)=>element==nombre);
+      if(busqueda != undefined){
+        return true;
+      }else{
+        return false;
+      }
+    }else{
+      return false;
+    }
+  }
 }
