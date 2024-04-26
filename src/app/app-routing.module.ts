7cf4b313cb4403a74fc77d0e857afc8791b6749d
Javier <javierdolorestolentino@gmail.com>
2024-04-25 18:45:42 -0600

    Importamos y anadimos el componente RegistroMateriasComponent en el archivo App.routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { HomeScreenComponent } from './screens/home-screen/home-screen.component
 import { AlumnosScreenComponent } from './screens/alumnos-screen/alumnos-screen.component';
 import { MaestrosScreenComponent } from './screens/maestros-screen/maestros-screen.component';
 import { AdminScreenComponent } from './screens/admin-screen/admin-screen.component';
+import { RegistroMateriasComponent } from './partials/registro-materias/registro-materias.component';
 
 const routes: Routes = [
   { path: '', component: LoginScreenComponent, pathMatch: 'full' },
@@ -15,6 +16,7 @@ const routes: Routes = [
   { path: 'administrador', component: AdminScreenComponent, pathMatch: 'full' },
   { path: 'alumnos', component: AlumnosScreenComponent, pathMatch: 'full' },
   { path: 'maestros', component: MaestrosScreenComponent, pathMatch: 'full' },
+  { path: 'registro-materias', component: RegistroMateriasComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
